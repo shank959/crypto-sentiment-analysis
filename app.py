b887b7d024e024825598e2490ce0a72d4b529121
shank959 <shanvkunzru@gmail.com>
2024-07-25 15:04:39 +0700

    added full functionality for the flask web app

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, request, jsonify, render_template
-from sentiment_analysis import sentiment_score
+from flask import Flask, request, render_template, redirect, url_for
+from sentiment_analysis import sentiment_scores, sentiment_count
 from webscrape import scrape_news
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    print("index page loaded")
     return render_template('index.html')
 
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    articles = request.json.get('articles', [])
-    sentiments = [sentiment_score(article) for article in articles]
-    return jsonify(sentiments)
+    coin = request.form['coin']
+    articles = scrape_news(coin_name=coin, num_pages=1)
+    sentiments = sentiment_scores(articles)
+    positive, neutral, negative = sentiment_count(sentiments)
+    return render_template('results.html', coin=coin, positive=positive, negative=negative, neutral=neutral)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,48 +1,61 @@
 from transformers import pipeline
 
 
-def sentiment_score(text):
+def sentiment_scores(articles):
 
     pipe = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis", device=-1)
 
-    sentences = text.split('.')
-    results = pipe(sentences)
-    sentence_count = len(sentences)
+    sentiment_table = {}
 
-    # # Display the results
-    # for sentence, result in zip(sentences, results):
-    #     print(f"Sentiment: {result['label']}, Score: {result['score']}\n")  
+    try:
+        for title, body in articles.items():
+            text = f"{title}. {body}"
 
-    # Determine Final Label and Score
-    sentiment_mapping = {
-        'positive': 1,
-        'neutral': 0,
-        'negative': -1
-    }
+            sentences = text.split('.')
+            results = pipe(sentences)
 
-    total_weighted_score = 0
-    total_confidence = 0
-    for result in results:
-        sentiment_score = sentiment_mapping[result['label']]
-        confidence_score = result['score']
-        weighted_score = sentiment_score * confidence_score
-        total_weighted_score += weighted_score
-        total_confidence += confidence_score
+            # # Display the results
+            # for sentence, result in zip(sentences, results):
+            #     print(f"Sentiment: {result['label']}, Score: {result['score']}\n")  
 
-    if  total_confidence != 0:
-        average_weighted_score = total_weighted_score / total_confidence
-    else:
-        average_weighted_score = 0
-    average_confidence = total_confidence / sentence_count
+            # Determine Final Label and Score
+            sentiment_mapping = {
+                'positive': 1,
+                'neutral': 0,
+                'negative': -1
+            }
 
-    if average_weighted_score > 0:
-        final_sentiment = 1
-    elif average_weighted_score < 0:
-        final_sentiment = -1
-    else:
-        final_sentiment = 0
+            total_weighted_score = 0
+            total_confidence = 0
+            for result in results:
+                sentiment_score = sentiment_mapping[result['label']]
+                confidence_score = result['score']
+                weighted_score = sentiment_score * confidence_score
+                total_weighted_score += weighted_score
+                total_confidence += confidence_score
 
-    return final_sentiment
-    # print(f"The Average Weighted Score is {average_weighted_score}")
+            average_weighted_score = (total_weighted_score / total_confidence) if total_confidence != 0 else 0
+            
+            final_sentiment = 1 if average_weighted_score > 0 else -1 if average_weighted_score < 0 else 0
+
+            sentiment_table[title] = final_sentiment
+
+        return sentiment_table
+    
+    except:
+        print("Error running sentiment analysis")
+        return {}
+
+
+def sentiment_count(sentiment_table):
+    positive, neutral, negative = 0, 0, 0
+    for sentiment in sentiment_table.values():
+        if sentiment == 1:
+            positive += 1
+        elif sentiment == -1:
+            negative += 1
+        else:
+            neutral += 1
+    return positive, neutral, negative
 
 
